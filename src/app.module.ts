448ead378b2e2869c3724a5d0b6bd31198434cac
Cao Minh Nguyen Cao Minh <c.m.nguyen-1@student.tudelft.nl>
2025-05-18 08:36:03 +0200

    Configure TypeORM with SQLite and update gitignore

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TicketsModule } from './tickets/tickets.module'; 
+import { EventsModule } from './events/events.module';   
+import { Event } from './events/event.entity';           
+import { Ticket } from './tickets/ticket.entity';      
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'ticket_management.sqlite',
+      entities: [Event, Ticket],
+      synchronize: true,
+    }),
+    EventsModule,
+    TicketsModule
+    ],
   controllers: [AppController],
   providers: [AppService],
 })
